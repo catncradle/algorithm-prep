81fac7e75302beaefb42713604b0b3a3fd5caba9
Jack Lye <catncradle@gmail.com>
2018-07-22 19:11:56 -0400

    BST code

--- data-structures/binarySearchTrees.js
+++ data-structures/binarySearchTrees.js
@@ -7,7 +7,6 @@ class BST {
   }
 
   addValue(value) {
-    // first we define a node
     const node = new BST(value);
     if (this.value >= node.value) {
       !this.left ? (this.left = node) : this.left.addValue(value);
@@ -16,5 +15,30 @@ class BST {
       !this.right ? (this.right = node) : this.right.addValue(value);
     }
     this.magnitude++;
+  }
+  contains(value) {
+    // base case if value is at the node return it
+    if (this.value === value) {
+      return true;
+    }
+    const direction = this.value >= value ? "left" : "right";
+    if (this[direction]) return this[direction].contains(value);
+    else return false;
+  }
+  depthFirstForEach(fn, opt = "in-order") {
+    if (opt === "pre-order") fn(this.value);
+    if (this.left) this.left.depthFirstForEach(fn, opt);
+    if (opt === "in-order") fn(this.value);
+    if (this.right) this.right.depthFirstForEach(fn, opt);
+    if (opt === "post-order") fn(this.value);
+  }
+  breadthFirstForEach(fn) {
+    const queue = [this];
+    while (queue.length) {
+      const current = queue.shift();
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+      fn(current.value);
+    }
   }
 }
