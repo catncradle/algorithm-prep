640c32c4f891245436e43cc579a9849b29b499b7
Kate Hee Kyun Yun <31594593+ggobugi27@users.noreply.github.com>
2018-08-02 18:15:02 -0400

    Create findMissingNum.js

---
+++ sorting-searching/findMissingNum.js
@@ -0,0 +1,12 @@
+// Given an array of incrementing numbers starting with 0, find a missing number. 
+// If no missing number is found, return the last element incremented by 1
+// Assume no more than one number is missing
+
+// Example
+
+// input : [0,1,2,3,4,5,7,8,9,10]
+// output : 6
+
+// input : [0,1,2,3,4,5,6,7,8,9,10]
+// output : 11
+
