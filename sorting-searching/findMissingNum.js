2752805b824b338154b452a6b68e3665085f70a0
Kate Hee Kyun Yun <31594593+ggobugi27@users.noreply.github.com>
2018-08-02 18:55:18 -0400

    Update findMissingNum.js

--- sorting-searching/findMissingNum.js
+++ sorting-searching/findMissingNum.js
@@ -10,3 +10,18 @@
 // input : [0,1,2,3,4,5,6,7,8,9,10]
 // output : 11
 
+
+function findMissingNum(arr){
+  let i = Math.floor(arr.length/2);
+  while (i < arr.length){
+    if (i < arr[i]){ // missing number is on the left side
+      if (arr[i-1] === i-1) return i;
+      else if (i-1 === 0) return 0;
+      else i--;
+    }
+    else if (i === arr[i]){ // no missing number of missing number is on right side
+      i++;
+    }
+  }
+  return arr.length //no number is missing
+}
