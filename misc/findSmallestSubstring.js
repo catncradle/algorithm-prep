5cb133308e62e79ff03e80f0df07a9c2b8418c01
Kate Hee Kyun Yun <31594593+ggobugi27@users.noreply.github.com>
2018-08-02 18:31:03 -0400

    Update findSmallestSubstring.js

--- misc/findSmallestSubstring.js
+++ misc/findSmallestSubstring.js
@@ -8,3 +8,37 @@
 
 // input:  arr = ['x','y','z'], str = "xyyzy"
 // output: "xyyz"
+
+function smallestSubstring(arr, s){
+  const arrObj = {};
+  arr.forEach(char => arrObj[char] = 0);
+  let headIdx = 0;
+  let result = '';
+  let uniqueCounter = 0;
+  for (let tailIdx=0; tailIdx<s.length; tailIdx++){
+    if (!arrObj.hasOwnProperty(s[tailIdx])){
+      continue;
+    }
+    else{
+      if (arrObj[s[tailIdx]]){
+        arrObj[s[tailIdx]] = arrObj[s[tailIdx]]+1;
+      }
+      else{
+        uniqueCounter++;
+        arrObj[s[tailIdx]] = arrObj[s[tailIdx]]+1;
+      }
+    }
+    while(uniqueCounter===arr.length){
+      let currentLength = tailIdx-headIdx+1
+      if (currentLength < result.length || !result.length){
+        result = s.slice(headIdx, tailIdx+1);
+      }
+      if (arrObj.hasOwnProperty(s[headIdx])){
+        arrObj[s[headIdx]] = arrObj[s[headIdx]]-1;
+        if (!arrObj[s[headIdx]]) uniqueCounter--;
+      }
+      headIdx++; 
+    }
+  }
+  return result;
+}
