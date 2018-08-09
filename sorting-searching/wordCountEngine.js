568ae92ea38e4c0d62e3e4bd6f0488b2a1385570
Jack Lye <catncradle@gmail.com>
2018-08-08 21:15:10 -0400

    added word Count Engine

---
+++ sorting-searching/wordCountEngine.js
@@ -0,0 +1,18 @@
+// Word Count Engine
+// Implement a document scanning function wordCountEngine, which receives a string document and returns a list of all unique words in it and their number of occurrences, sorted by the number of occurrences in a descending order. If two or more words have the same count, they should be sorted according to their order in the original sentence. Assume that all letters are in english alphabet. You function should be case-insensitive, so for instance, the words “Perfect” and “perfect” should be considered the same word.
+
+// The engine should strip out punctuation (even in the middle of a word) and use whitespaces to separate words.
+
+// Analyze the time and space complexities of your solution. Try to optimize for time while keeping a polynomial space complexity.
+
+// Examples:
+
+// input:  document = "Practice makes perfect. you'll only
+//                     get Perfect by practice. just practice!"
+
+// output: [ ["practice", "3"], ["perfect", "2"],
+//           ["makes", "1"], ["youll", "1"], ["only", "1"],
+//           ["get", "1"], ["by", "1"], ["just", "1"] ]
+
+// Input: "To be, or not to be, that is the question:"
+// output:[["to","2"],["be","2"],["or","1"],["not","1"],["that","1"],["is","1"],["the","1"],["question","1"]]
